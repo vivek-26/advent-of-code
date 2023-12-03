c5365f557b41368b734d312f17917552b74c16ac
Vivek Kumar <vivek.26@outlook.com>
2023-12-03 16:30:20 +0530

    add day 3, part 2 solution

---
+++ aoc-2023/day-03/src/bin/part_two.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+use aoc;
+
+const DIR: &[(isize, isize)] = &[
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+fn main() {
+    let board: Vec<Vec<char>> = aoc::read_input_lines(3)
+        .iter()
+        .map(|line| line.chars().map(|c| c).collect::<Vec<char>>())
+        .collect();
+
+    let rows = board.len();
+    let cols = board[0].len();
+    let mut gear_rations = HashMap::new();
+
+    for r in 0..rows {
+        let mut num = 0;
+        let mut is_valid = false;
+        let (mut gear_row, mut gear_col) = (0, 0);
+
+        for c in 0..cols {
+            if !board[r][c].is_ascii_digit() {
+                if is_valid {
+                    is_valid = false;
+
+                    gear_rations
+                        .entry((gear_row, gear_col))
+                        .or_insert(vec![])
+                        .push(num);
+                }
+
+                num = 0;
+                continue;
+            }
+
+            num *= 10;
+            num += (board[r][c].to_digit(10).unwrap()) as u64;
+
+            if !is_valid {
+                for (dr, dc) in DIR.iter().copied() {
+                    let Some(rx) = r.checked_add_signed(dr) else {
+                        continue;
+                    };
+                    let Some(cx) = c.checked_add_signed(dc) else {
+                        continue;
+                    };
+                    if rx >= rows || cx >= cols {
+                        continue;
+                    }
+
+                    if board[rx][cx] == '*' {
+                        gear_row = rx;
+                        gear_col = cx;
+                        is_valid = true;
+                    }
+                }
+            }
+        }
+
+        // leftover number at the end of the row
+        if is_valid {
+            gear_rations
+                .entry((gear_row, gear_col))
+                .or_insert(vec![])
+                .push(num);
+        }
+    }
+
+    let answer: u64 = gear_rations
+        .into_values()
+        .filter(|x| x.len() == 2)
+        .map(|x| x[0] * x[1])
+        .sum();
+
+    println!("{answer}");
+}
