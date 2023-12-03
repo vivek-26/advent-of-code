40874e39950ce5be664d4192a867fe9dadb3faee
Vivek Kumar <vivek.26@outlook.com>
2023-12-03 16:29:52 +0530

    add day 3, part 1 solution

---
+++ aoc-2023/day-03/src/bin/part_one.rs
@@ -0,0 +1,69 @@
+use aoc;
+
+const DIR: &[(isize, isize)] = &[
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+fn main() {
+    let board: Vec<Vec<char>> = aoc::read_input_lines(3)
+        .iter()
+        .map(|line| line.chars().map(|c| c).collect::<Vec<char>>())
+        .collect();
+
+    let rows = board.len();
+    let cols = board[0].len();
+    let mut answer = 0;
+
+    for r in 0..rows {
+        let mut num = 0;
+        let mut is_valid = false;
+
+        for c in 0..cols {
+            if !board[r][c].is_ascii_digit() {
+                if is_valid {
+                    answer += num;
+                    is_valid = false;
+                }
+
+                num = 0;
+                continue;
+            }
+
+            num *= 10;
+            num += (board[r][c].to_digit(10).unwrap()) as u64;
+
+            if !is_valid {
+                for (dr, dc) in DIR.iter().copied() {
+                    let Some(rx) = r.checked_add_signed(dr) else {
+                        continue;
+                    };
+                    let Some(cx) = c.checked_add_signed(dc) else {
+                        continue;
+                    };
+
+                    if rx >= rows || cx >= cols {
+                        continue;
+                    }
+
+                    if board[rx][cx] != '.' && !board[rx][cx].is_ascii_digit() {
+                        is_valid = true;
+                    }
+                }
+            }
+        }
+
+        // leftover number at the end of the row
+        if is_valid {
+            answer += num;
+        }
+    }
+
+    println!("{answer}");
+}
