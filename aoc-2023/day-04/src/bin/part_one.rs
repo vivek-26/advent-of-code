569f0443d16a1e1ebd81b5c72622d70d51dcaa31
Vivek Kumar <vivek.26@outlook.com>
2023-12-04 17:19:31 +0530

    add day 4, part 1 solution

---
+++ aoc-2023/day-04/src/bin/part_one.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+
+fn main() {
+    let mut sum = 0_u32;
+
+    for line in aoc::read_input_lines(4) {
+        let numbers: Vec<&str> = line.split(':').nth(1).unwrap().split('|').collect();
+
+        let winning_numbers: HashSet<u32> = numbers[0]
+            .split_whitespace()
+            .filter_map(|n| n.parse::<u32>().ok())
+            .collect();
+
+        let matches = numbers[1]
+            .split_whitespace()
+            .filter_map(|n| n.parse::<u32>().ok())
+            .filter(|n| winning_numbers.contains(n))
+            .count();
+
+        if let Some(matches) = matches.checked_sub(1) {
+            sum += u32::pow(2, matches as u32);
+        }
+    }
+
+    println!("{}", sum);
+}
