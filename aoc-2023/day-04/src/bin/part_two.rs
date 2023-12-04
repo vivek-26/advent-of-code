6d3657010fb8f1f2173b237f6f627d32f61a1a73
Vivek Kumar <vivek.26@outlook.com>
2023-12-04 17:19:48 +0530

    add day 4, part 2 solution

---
+++ aoc-2023/day-04/src/bin/part_two.rs
@@ -0,0 +1,31 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let mut sum = 0_u32;
+    let mut copies = HashMap::new();
+    copies.insert(1, 1);
+
+    for (idx, line) in aoc::read_input_lines(4).iter().enumerate() {
+        let numbers: Vec<&str> = line.split(':').nth(1).unwrap().split('|').collect();
+
+        let winning_numbers: HashSet<u32> = numbers[0]
+            .split_whitespace()
+            .filter_map(|n| n.parse::<u32>().ok())
+            .collect();
+
+        let matches = numbers[1]
+            .split_whitespace()
+            .filter_map(|n| n.parse::<u32>().ok())
+            .filter(|n| winning_numbers.contains(n))
+            .count();
+
+        let current_multiplier = *copies.entry(idx).or_insert(1);
+        sum += current_multiplier;
+
+        for i in 1..matches + 1 {
+            *copies.entry(idx + i).or_insert(1) += current_multiplier;
+        }
+    }
+
+    println!("{}", sum);
+}
