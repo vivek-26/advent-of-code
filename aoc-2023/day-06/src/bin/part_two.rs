1fed349711d9447570509bc7a2af67380b7afa0f
Vivek Kumar <vivek.26@outlook.com>
2023-12-06 19:03:12 +0530

    add day 6 part 2 solution

---
+++ aoc-2023/day-06/src/bin/part_two.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let lines = aoc::read_input_lines(6);
+    let mut lines_iter = lines.iter();
+
+    let recorded_time = parse_input(lines_iter.next().unwrap());
+    let recorded_distance = parse_input(lines_iter.next().unwrap());
+
+    let mut beat_record = 0;
+    for t in 0..=recorded_time {
+        if t * (recorded_time - t) > recorded_distance {
+            beat_record += 1;
+        }
+    }
+
+    println!("{}", beat_record);
+}
+
+fn parse_input(input: &str) -> usize {
+    input
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse()
+        .unwrap_or(0)
+}
