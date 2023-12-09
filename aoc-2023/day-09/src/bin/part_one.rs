36974f341f8c306819728613ccbb7c8331a2ad7a
Vivek Kumar <vivek.26@outlook.com>
2023-12-09 21:05:46 +0530

    add day 9, part 1 solution

--- aoc-2023/day-09/src/bin/part_one.rs
+++ aoc-2023/day-09/src/bin/part_one.rs
@@ -1,1 +1,23 @@
-fn main() {}
+fn main() {
+    let lines = aoc::read_input_lines(9);
+    let mut histories: Vec<Vec<isize>> = lines
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|num_str| num_str.parse().ok())
+                .collect()
+        })
+        .collect();
+
+    for history in &mut histories {
+        let steps = history.len() - 2;
+        for step in 0..steps {
+            for idx in 0..history.len() - step - 1 {
+                history[idx] = history[idx + 1] - history[idx];
+            }
+        }
+    }
+
+    let answer: isize = histories.iter().flat_map(|history| history.iter()).sum();
+    println!("{answer}");
+}
