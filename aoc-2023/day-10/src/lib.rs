eb976fe34d7ad0b8b2bb4694a9677c659f129aa0
Vivek Kumar <vivek.26@outlook.com>
2023-12-12 11:19:46 +0530

    refactor lib

--- aoc-2023/day-10/src/lib.rs
+++ aoc-2023/day-10/src/lib.rs
@@ -1,35 +1,4 @@
 #[derive(Debug, PartialEq)]
-pub enum Direction {
-    Left,
-    Right,
-    Up,
-    Down,
-    None,
-}
-
-impl Direction {
-    pub fn get_coords(&self) -> (isize, isize) {
-        match self {
-            Direction::Left => (0, -1),
-            Direction::Right => (0, 1),
-            Direction::Up => (-1, 0),
-            Direction::Down => (1, 0),
-            Direction::None => (0, 0),
-        }
-    }
-
-    pub fn opposite(&self) -> Self {
-        match self {
-            Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left,
-            Direction::Up => Direction::Down,
-            Direction::Down => Direction::Up,
-            Direction::None => Direction::None,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
 pub enum PipeDirection {
     Vertical,    // |
     Horizontal,  // -
@@ -52,6 +21,47 @@ impl PipeDirection {
             PipeDirection::SouthToEast => [Direction::Down, Direction::Right],
             PipeDirection::Ground => [Direction::None, Direction::None],
             PipeDirection::StartingPos => [Direction::None, Direction::None],
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+    None,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Self {
+        match self {
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::None => Direction::None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Coordinate(pub isize, pub isize);
+
+impl Coordinate {
+    pub fn add(&mut self, other: Self) {
+        self.0 += other.0;
+        self.1 += other.1;
+    }
+
+    pub fn go_to(&self, direction: &Direction) -> Self {
+        match direction {
+            Direction::Left => Coordinate(self.0, self.1 - 1),
+            Direction::Right => Coordinate(self.0, self.1 + 1),
+            Direction::Up => Coordinate(self.0 - 1, self.1),
+            Direction::Down => Coordinate(self.0 + 1, self.1),
+            Direction::None => Coordinate(self.0, self.1),
         }
     }
 }
