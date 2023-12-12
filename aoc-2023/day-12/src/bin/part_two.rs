3665cef4e15ef935f03c8e39b33ef77d1b92b483
Vivek Kumar <vivek.26@outlook.com>
2023-12-12 21:18:01 +0530

    add day 12, part 2 solution

--- aoc-2023/day-12/src/bin/part_two.rs
+++ aoc-2023/day-12/src/bin/part_two.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use itertools::Itertools;
+
+use day_12::possible_ways;
+
+fn main() {
+    let lines = aoc::read_input_lines(12);
+    let answer = lines
+        .iter()
+        .map(|line| {
+            let (spring, counts) = line.split_once(' ').unwrap();
+
+            let spring = std::iter::once(spring).cycle().take(5).join("?");
+
+            let counts = counts
+                .split(',')
+                .map(|number| number.parse::<usize>().unwrap())
+                .collect_vec();
+            let n = counts.len();
+
+            possible_ways(&spring, counts.into_iter().cycle().take(5 * n))
+        })
+        .sum::<usize>();
+
+    println!("{answer}");
+}
