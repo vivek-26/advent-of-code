0c2d85aa6eadf5b2052330d41ffd5752dbaed878
Vivek Kumar <vivek.26@outlook.com>
2023-12-12 21:17:52 +0530

    add day 12, part 1 solution

--- aoc-2023/day-12/src/bin/part_one.rs
+++ aoc-2023/day-12/src/bin/part_one.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use day_12::possible_ways;
+
+fn main() {
+    let lines = aoc::read_input_lines(12);
+    let answer = lines
+        .iter()
+        .map(|line| {
+            let (spring, counts) = line.split_once(' ').unwrap();
+            let counts = counts
+                .split(',')
+                .map(|number| number.parse::<usize>().unwrap());
+
+            possible_ways(spring, counts)
+        })
+        .sum::<usize>();
+
+    println!("{answer}");
+}
