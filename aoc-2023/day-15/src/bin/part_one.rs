d07cd0f872ff627dce23d3d7e438fa55caeba071
Vivek Kumar <vivek.26@outlook.com>
2023-12-15 16:37:21 +0530

    add day 15, part 1 solution

--- aoc-2023/day-15/src/bin/part_one.rs
+++ aoc-2023/day-15/src/bin/part_one.rs
@@ -1,1 +1,16 @@
-fn main() {}
+fn main() {
+    let input = aoc::read_input(15);
+    let answer: usize = input.split(',').map(|step| hash(step) as usize).sum();
+    println!("{answer}");
+}
+
+fn hash(step: &str) -> u8 {
+    let curr = step.chars().fold(0_u16, |mut acc, ch| {
+        acc += ch as u16;
+        acc *= 17;
+        acc %= 256;
+        acc
+    });
+
+    curr as u8
+}
