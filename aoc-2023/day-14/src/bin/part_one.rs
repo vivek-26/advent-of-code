779f5f52a927919f7c2fe1db16545ee350d4a362
Vivek Kumar <vivek.26@outlook.com>
2023-12-15 11:19:16 +0530

    optimise solution

--- aoc-2023/day-14/src/bin/part_one.rs
+++ aoc-2023/day-14/src/bin/part_one.rs
@@ -3,7 +3,7 @@ use day_14::Landscape;
 fn main() {
     let mut grid = parse_input(aoc::read_input_lines(14));
     slide_north(&mut grid);
-    println!("{}", total_load(&grid));
+    println!("{}", total_load_north_beam(&grid));
 }
 
 fn parse_input(input: Vec<String>) -> Vec<Vec<Landscape>> {
@@ -30,32 +30,30 @@ fn next_empty_slot(grid: &Vec<Vec<Landscape>>, row: usize, col: usize) -> usize 
 
 fn slide_north(grid: &mut Vec<Vec<Landscape>>) {
     for col in 0..grid[0].len() {
-        for _ in 0..grid.len() {
-            let mut ptr = next_empty_slot(grid, 0, col);
-            let mut curr = ptr + 1;
+        let mut ptr = next_empty_slot(grid, 0, col);
+        let mut curr = ptr + 1;
 
-            while curr < grid.len() {
-                match grid[curr][col] {
-                    Landscape::RoundedRock => {
-                        grid[ptr][col] = Landscape::RoundedRock;
-                        grid[curr][col] = Landscape::EmptySpace;
-                        ptr += 1;
-                        curr += 1;
-                    }
-                    Landscape::CubeRock => {
-                        ptr = next_empty_slot(grid, curr, col);
-                        curr = ptr + 1;
-                    }
-                    Landscape::EmptySpace => {
-                        curr += 1;
-                    }
-                };
-            }
+        while curr < grid.len() {
+            match grid[curr][col] {
+                Landscape::RoundedRock => {
+                    grid[ptr][col] = Landscape::RoundedRock;
+                    grid[curr][col] = Landscape::EmptySpace;
+                    ptr += 1;
+                    curr += 1;
+                }
+                Landscape::CubeRock => {
+                    ptr = next_empty_slot(grid, curr, col);
+                    curr = ptr + 1;
+                }
+                Landscape::EmptySpace => {
+                    curr += 1;
+                }
+            };
         }
     }
 }
 
-fn total_load(grid: &Vec<Vec<Landscape>>) -> usize {
+fn total_load_north_beam(grid: &Vec<Vec<Landscape>>) -> usize {
     grid.iter().enumerate().fold(0, |total, (row, line)| {
         total
             + line
