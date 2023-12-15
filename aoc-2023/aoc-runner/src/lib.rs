bb0131c49fad5bde954d9fa509015e21e57b5d59
Vivek Kumar <vivek.26@outlook.com>
2023-12-15 22:07:14 +0530

    add aoc-runner crate with timeit proc macro

---
+++ aoc-2023/aoc-runner/src/lib.rs
@@ -0,0 +1,27 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Ident, ItemFn};
+
+#[proc_macro_attribute]
+pub fn timeit(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut aoc_solution = parse_macro_input!(item as ItemFn);
+    aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
+
+    let expanded = quote! {
+        #aoc_solution
+        fn main() {
+            let start = std::time::Instant::now();
+            let answer = aoc_solution();
+            let elapsed = start.elapsed();
+            println!("Answer: {}", answer);
+
+            if elapsed.as_millis() > 0 {
+                println!("Time: {}ms", elapsed.as_millis());
+              } else {
+                println!("Time: {}μs", elapsed.as_micros());
+              }
+        }
+    };
+
+    expanded.into()
+}
