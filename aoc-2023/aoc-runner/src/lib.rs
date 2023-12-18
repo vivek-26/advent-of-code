22f0ff0054e055588e18e6b1e54ef77b0c051714
Vivek Kumar <vivek.26@outlook.com>
2023-12-18 09:24:15 +0530

    pass input as parameter

--- aoc-2023/aoc-runner/src/lib.rs
+++ aoc-2023/aoc-runner/src/lib.rs
@@ -1,9 +1,16 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Ident, ItemFn};
+use syn::{parse_macro_input, Ident, ItemFn, LitInt};
 
 #[proc_macro_attribute]
-pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn main(args: TokenStream, item: TokenStream) -> TokenStream {
+    let day = parse_macro_input!(args as LitInt);
+    let day = day.base10_parse::<u8>().unwrap();
+
+    let input_path = get_input_path(day);
+    let input = std::fs::read_to_string(input_path).unwrap();
+    let input = input.as_str();
+
     let mut aoc_solution = parse_macro_input!(item as ItemFn);
     aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
 
@@ -11,7 +18,7 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
         #aoc_solution
         fn main() {
             let start = std::time::Instant::now();
-            let answer = aoc_solution();
+            let answer = aoc_solution(#input);
             let elapsed = start.elapsed();
             println!("Answer: {}", answer);
 
@@ -24,4 +31,11 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     expanded.into()
+}
+
+fn get_input_path(day: u8) -> std::path::PathBuf {
+    let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.pop();
+    path.push("inputs");
+    path.join(format!("{:02}.in", day))
 }
