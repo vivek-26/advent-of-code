b3eaaa1516a0b23bf069d0c2e9d7c8d73595a605
Vivek Kumar <vivek.26@outlook.com>
2023-12-18 10:32:31 +0530

    use input received from proc macro

--- aoc-2023/day-12/src/bin/part_one.rs
+++ aoc-2023/day-12/src/bin/part_one.rs
@@ -1,10 +1,9 @@
 use day_12::possible_ways;
 
-#[aoc_runner::main]
-fn main() -> usize {
-    let lines = aoc::read_input(12);
-    lines
-        .split('\n')
+#[aoc_runner::main(12)]
+fn main(input: &str) -> usize {
+    input
+        .lines()
         .map(|line| {
             let (spring, counts) = line.split_once(' ').unwrap();
             let counts = counts
--- aoc-2023/day-12/src/bin/part_two.rs
+++ aoc-2023/day-12/src/bin/part_two.rs
@@ -2,11 +2,10 @@ use itertools::Itertools;
 
 use day_12::possible_ways;
 
-#[aoc_runner::main]
-fn main() -> usize {
-    let lines = aoc::read_input(12);
-    lines
-        .split('\n')
+#[aoc_runner::main(12)]
+fn main(input: &str) -> usize {
+    input
+        .lines()
         .map(|line| {
             let (spring, counts) = line.split_once(' ').unwrap();
 
