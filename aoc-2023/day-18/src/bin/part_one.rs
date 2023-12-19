7f0f3bf9f1e404b8d1824a9ce0c75de60c07be13
Vivek Kumar <vivek.26@outlook.com>
2023-12-19 09:00:52 +0530

    add day 18, part 1 solution

--- aoc-2023/day-18/src/bin/part_one.rs
+++ aoc-2023/day-18/src/bin/part_one.rs
@@ -1,4 +1,27 @@
 #[aoc_runner::main(18)]
-fn main(input: &str) -> usize {
-    0
+fn main(input: &str) -> isize {
+    let instructions = input.split('\n').map(|line| {
+        let (len, _) = line[2..].split_once(' ').unwrap();
+        (line.as_bytes()[0], len.parse::<isize>().unwrap())
+    });
+
+    shoelace_formula(instructions)
+}
+
+fn shoelace_formula(instructions: impl Iterator<Item = (u8, isize)>) -> isize {
+    let (mut area, mut r, mut c) = (0, 0, 0);
+    for (direction, len) in instructions {
+        let (rr, cc) = (r, c);
+        match direction {
+            b'U' => r -= len,
+            b'R' => c += len,
+            b'D' => r += len,
+            b'L' => c -= len,
+            _ => unreachable!(),
+        };
+
+        area += (c + cc) * (r - rr) + len;
+    }
+
+    area / 2 + 1
 }
