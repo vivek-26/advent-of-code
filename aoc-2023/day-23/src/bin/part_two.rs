862181a7704f99bd45c8e4bb3530f63794cb8090
Vivek Kumar <vivek.26@outlook.com>
2023-12-23 21:11:00 +0530

    refactor and optimise solution

--- aoc-2023/day-23/src/bin/part_two.rs
+++ aoc-2023/day-23/src/bin/part_two.rs
@@ -1,41 +1,50 @@
 use hashbrown::HashMap;
+use itertools::Itertools;
+
+const NEIGHBORS: &[(isize, isize)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
 
 #[aoc_runner::main(23)]
 fn main(input: &str) -> usize {
-    let grid = input.lines().map(str::as_bytes).collect::<Vec<_>>();
+    let grid = input.split('\n').map(str::as_bytes).collect::<Vec<_>>();
     let mut graph = HashMap::<_, Vec<_>>::new();
 
-    for r in 0..grid.len() {
-        for c in 0..grid[0].len() {
-            if grid[r][c] == b'#' {
+    for (r, c) in (0..grid.len()).cartesian_product(0..grid[0].len()) {
+        let neighbors = match grid[r][c] {
+            b'#' => continue,
+            _ => NEIGHBORS,
+        };
+
+        let e = graph.entry((r, c)).or_default();
+        for (dr, dc) in neighbors {
+            let rr = (r as isize + dr) as usize;
+            let cc = (c as isize + dc) as usize;
+            let Some(&tile) = grid.get(rr).and_then(|row| row.get(cc)) else {
                 continue;
-            }
+            };
 
-            let neighbors: &[_] = &[(-1, 0), (1, 0), (0, -1), (0, 1)];
-            let e = graph.entry((r, c)).or_default();
-
-            for (dr, dc) in neighbors {
-                let rr = (r as isize + dr) as usize;
-                let cc = (c as isize + dc) as usize;
-                let Some(&tile) = grid.get(rr).and_then(|row| row.get(cc)) else {
-                    continue;
-                };
-                if tile != b'#' {
-                    e.push((rr, cc, 1));
-                }
+            if tile != b'#' {
+                e.push((rr, cc, 1));
             }
         }
     }
 
-    while let Some((&(r, c), _)) = graph.iter().find(|(_, n)| n.len() == 2) {
+    let corridors = graph
+        .iter()
+        .filter(|(_, n)| n.len() == 2)
+        .map(|(&node, _)| node)
+        .collect::<Vec<_>>();
+
+    for (r, c) in corridors {
         let neighbors = graph.remove(&(r, c)).unwrap();
         let (r1, c1, d1) = neighbors[0];
         let (r2, c2, d2) = neighbors[1];
-        let n1 = graph.entry((r1, c1)).or_default();
+        let n1 = graph.get_mut(&(r1, c1)).unwrap();
+
         if let Some(i) = n1.iter().position(|&(rr, cc, _)| (rr, cc) == (r, c)) {
             n1[i] = (r2, c2, d1 + d2);
         }
-        let n2 = graph.entry((r2, c2)).or_default();
+
+        let n2 = graph.get_mut(&(r2, c2)).unwrap();
         if let Some(i) = n2.iter().position(|&(rr, cc, _)| (rr, cc) == (r, c)) {
             n2[i] = (r1, c1, d1 + d2);
         }
