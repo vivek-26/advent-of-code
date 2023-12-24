272f378e8b79232234c13f2586e1c6a80645bc42
Vivek Kumar <vivek.26@outlook.com>
2023-12-24 14:04:35 +0530

    add day 24, part 1 solution

--- aoc-2023/day-24/src/bin/part_one.rs
+++ aoc-2023/day-24/src/bin/part_one.rs
@@ -1,4 +1,64 @@
+use itertools::Itertools;
+
 #[aoc_runner::main(24)]
 fn main(input: &str) -> usize {
-    0
+    let hailstones = input
+        .lines()
+        .map(|line| {
+            let (a, b) = line.split_once(" @ ").unwrap();
+            let (x, y, z) = a
+                .split(", ")
+                .map(|w| w.parse::<f64>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            let (dx, dy, dz) = b
+                .split(", ")
+                .map(|w| w.trim().parse::<f64>().unwrap())
+                .collect_tuple()
+                .unwrap();
+            ((x, y, z), (dx, dy, dz))
+        })
+        .collect::<Vec<_>>();
+
+    find_intersections(&hailstones, 200000000000000.0, 400000000000000.0)
+}
+
+fn find_intersections(
+    hailstones: &[((f64, f64, f64), (f64, f64, f64))],
+    start: f64,
+    end: f64,
+) -> usize {
+    hailstones
+        .iter()
+        .tuple_combinations()
+        .filter(
+            |(&((x1, y1, _), (dx1, dy1, _)), &((x2, y2, _), (dx2, dy2, _)))| {
+                let Some((x, y)) = intersection((x1, y1, dx1, dy1), (x2, y2, dx2, dy2)) else {
+                    return false;
+                };
+
+                if dx1.signum() != (x - x1).signum() || dx2.signum() != (x - x2).signum() {
+                    return false;
+                }
+
+                (start..=end).contains(&x) && (start..=end).contains(&y)
+            },
+        )
+        .count()
+}
+
+fn intersection(
+    (x1, y1, dx1, dy1): (f64, f64, f64, f64),
+    (x2, y2, dx2, dy2): (f64, f64, f64, f64),
+) -> Option<(f64, f64)> {
+    let m1 = dy1 / dx1;
+    let m2 = dy2 / dx2;
+    if (m2 - m1).abs() < f64::EPSILON {
+        return None;
+    }
+
+    let x = (m1 * x1 - m2 * x2 + y2 - y1) / (m1 - m2);
+    let y = (m1 * m2 * (x2 - x1) + m2 * y1 - m1 * y2) / (m2 - m1);
+
+    Some((x, y))
 }
