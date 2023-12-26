37766bd95a00f666901ec976caa0a461fd35acb6
Vivek Kumar <vivek.26@outlook.com>
2023-12-26 15:56:51 +0530

    add day 25, part 1 solution

--- aoc-2023/day-25/src/bin/part_one.rs
+++ aoc-2023/day-25/src/bin/part_one.rs
@@ -1,4 +1,47 @@
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+
 #[aoc_runner::main(25)]
 fn main(input: &str) -> usize {
-    0
+    let mut graph = HashMap::<_, HashSet<_>>::new();
+    let mut edges = HashSet::new();
+    for line in input.lines() {
+        let (a, rest) = line.split_once(": ").unwrap();
+        for b in rest.split(' ') {
+            graph.entry(a).or_default().insert(b);
+            graph.entry(b).or_default().insert(a);
+            edges.insert(if a < b { (a, b) } else { (b, a) });
+        }
+    }
+
+    let mut dot = String::from("graph {\n");
+    for (a, b) in edges.iter().sorted() {
+        dot += &format!("  {} -- {};\n", a, b);
+    }
+    dot += "}";
+
+    // Run the following to visualize the graph:
+    //   dot -Tsvg -Kneato graph.dot > graph.svg
+    // Manually find the three edges.
+    // If dot command is not available, install graphviz.
+    std::fs::write("graph.dot", dot).unwrap();
+
+    for (a, b) in [("grd", "tqr"), ("dlv", "tqh"), ("bmd", "ngp")] {
+        graph.get_mut(a).unwrap().remove(b);
+        graph.get_mut(b).unwrap().remove(a);
+    }
+
+    let size = component_size(&graph, "tqr");
+    (graph.len() - size) * size
+}
+
+fn component_size(graph: &HashMap<&str, HashSet<&str>>, a: &str) -> usize {
+    let (mut seen, mut s) = (HashSet::new(), vec![a]);
+    while let Some(x) = s.pop() {
+        if seen.insert(x) {
+            s.extend(&graph[x]);
+        }
+    }
+
+    seen.len()
 }
